e2473d92ac849c5cc43fbaa3ec85dbf161eddcbc
bullcut <sundaniel3@gmail.com>
2015-07-28 23:17:32 -0700

    Rewrite FSR loop
    cleaned up code, added serial and changed baud rate to 115200. Uncomment
    #define SERIAL_ON to enable serial communication

--- Movement/Arduino_Uno_--_Inputs/Arduino_Uno_--_Inputs.ino
+++ Movement/Arduino_Uno_--_Inputs/Arduino_Uno_--_Inputs.ino
@@ -1,41 +1,63 @@
-int senseNum[] = {A0, A2, A3, A5, A1, A4};
-int LEDNum[] = {2, 4, 5, 7, 3, 6};
-int threshold[] = {5, 5, 5, 5, 5, 5};
-int x = 0;
+// Code for reading analog inputs from FSRs, outputting to serial and piping them to the Arbotix.
+// Used because the Arbotix was having some issues reading analog values.
 
-//Right Front == 0
-//Right Rear == 1
-//Left Front == 2
-//Left Rear == 3
-//Right Middle == 4
-//Left Middle == 5
+//#define SERIAL_ON
+
+int senseNum[] = {A0, A2, A3, A5, A1, A4}; // analog pins
+int LEDNum[] = {2, 4, 5, 7, 3, 6}; // digital pins
+int threshold[] = {5, 5, 5, 5, 5, 5}; // threshold for each of the pins
+
+char* legs[] = {" Right Front: ", " Right Rear: ", " Left Front: ", " Left Rear: ", " Right Middle: ", " Left Middle: "};
+
+// Right Front  == 0
+// Right Rear   == 1
+// Left Front   == 2
+// Left Rear    == 3
+// Right Middle == 4
+// Left Middle  == 5
 
 void setup(){
-  for(x = 0; x < 6; x++)
+  // initialize all of the pins
+  for(int x = 0; x < 6; x++)
   {
     pinMode(senseNum[x], INPUT); //for the FSRs
     pinMode(LEDNum[x], OUTPUT); //for the LEDs
     pinMode(LEDNum[x] + 6, OUTPUT); //for the Arbotix
   }
-  Serial.begin(9600);
+  
+
+  Serial.begin(115200);
+
 }
 
+unsigned time = millis();
+
 void loop(){
-  if(x == 0){
-    Serial.println(" ");
-  } else{
-    Serial.print(" ");
+  // Loop Timer
+  Serial.print("EXEC TIME: "); Serial.print(millis()-time);
+  time = millis();
+  
+  //
+  for(int x = 0; x < 6; x++){
+    // print out leg enum
+    int reading = analogRead(senseNum[x]);
+
+#ifdef SERIAL_ON
+    Serial.print(legs[x]);
+    Serial.print(reading);
+#endif 
+    
+    // if above threshold, turn on LED and send HIGH to Arbotix
+    if (reading > threshold[x])
+    {
+      digitalWrite(LEDNum[x], HIGH);
+      digitalWrite(LEDNum[x] + 6, HIGH);
+    }
+    else
+    {
+      digitalWrite(LEDNum[x], LOW);
+      digitalWrite(LEDNum[x] + 6, LOW);
+    }
   }
-  Serial.print(analogRead(senseNum[x]));
-  if (analogRead(senseNum[x]) > threshold[x])
-  {
-    digitalWrite(LEDNum[x], HIGH);
-    digitalWrite(LEDNum[x] + 6, HIGH);
-  }
-  else
-  {
-    digitalWrite(LEDNum[x], LOW);
-    digitalWrite(LEDNum[x] + 6, LOW);
-  }
-  x = (x+1)%6;
+  Serial.println();
 }
